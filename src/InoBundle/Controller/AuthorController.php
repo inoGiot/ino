2e0d0bd1ec73778ff9285c4d3c010cad1208fb55
ino <inogiot@gmail.com>
2016-08-31 14:52:22 +0200

    Add author controller

---
+++ src/InoBundle/Controller/AuthorController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace InoBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+/**
+ * @Template()
+ */
+class AuthorController extends Controller
+{
+    public function indexAction()
+    {
+        return [];
+    }
+
+}
